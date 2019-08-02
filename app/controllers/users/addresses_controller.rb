5bb3a911150927fb2eded6b190bafdbf6078acb4
kobayashi.fuga <kobayashi.fuga.infratop@gmail.com>
2019-08-02 09:33:15 +0000

    order-controller

--- app/controllers/users/addresses_controller.rb
+++ app/controllers/users/addresses_controller.rb
@@ -1,2 +1,11 @@
 class Users::AddressesController < ApplicationController
+    def create
+        address = Address.new(address_params)
+        address.save
+    end
+    
+    private
+        def address_params
+            params.require(:address).permit(:payment, :postal_code, :address)
+        end
 end
--- app/controllers/users/orders_controller.rb
+++ app/controllers/users/orders_controller.rb
@@ -9,12 +9,20 @@ class Users::OrdersController < ApplicationController
     def create
         order = Order.new(order_params)
         order.save
-        redirect_to users_order_details_path
+        order_pay = Order.new(order_pay_params)
+        if order_pay.save
+            redirect_to users_order_details_path
+        else
+            redirect_to new_users_orders_path
+        end
     end
     
     private
     
     def order_params
-      params.require(:order).permit(:total_price, :payment, :postal_code, :address, order_attributes: [:id, :cart_item_id])
+      params.require(:order).permit(:total_price, order_attributes: [:id, :cart_item_id])
     end
+    def order_pay_params
+      params.require(:order).permit(:payment, :postal_code, :address)
+    end     
 end
